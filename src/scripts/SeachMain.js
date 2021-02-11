4756de2ea519307fa5518f3dee5cb1c6d3c9cfdf
Dave Michelson <daveism@gmail.com>
2021-02-11 08:27:24 -0500

    preping for new maps

--- src/scripts/SeachMain.js
+++ src/scripts/SeachMain.js
@@ -648,7 +648,12 @@ export default function SearchMain() {
   };
 
   // get with or without target array used to retreive the correct map
-  const getWithORwithout = (withORwithout) => (withORwithout === 'with' ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20] : [0]);
+  const getWithORwithout = (withORwithout) => (withORwithout === 'with' ? [
+     1, 2, 3, 4, 5, 6, 7, 8, 9,
+     10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+     20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+     30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+     40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, ] : [0]);
 
   const nextMapURL = (props) => {
     // get target name none or 1-20
